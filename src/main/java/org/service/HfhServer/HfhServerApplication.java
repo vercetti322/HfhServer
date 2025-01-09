5e8d1e7f911a623910486c8b7500466217e67bcd
vercetti322 <vercettitommy322@gmail.com>
2025-01-09 11:10:35 +0530

    project init!

---
+++ src/main/java/org/service/HfhServer/HfhServerApplication.java
@@ -0,0 +1,11 @@
+package org.service.HfhServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfhServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HfhServerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/service/HfhServer/HfhServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.service.HfhServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HfhServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
